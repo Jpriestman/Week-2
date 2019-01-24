ef6dc6b0299d3b5f0638c0d84f62d35bb14244a2
Jordan Priestman <jordanpriestman@Jordans-MBP.fc.ca>
2019-01-24 15:19:17 -0500

    Finsihed Routing

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
 (() => {
     // Create a component first
-    const UserComponent = {
-        props: ['name', 'role'],
 
-        template: "#userstemplate",
-
-        created: function() {
-            console.log("I'm Aliive");
-        },
-
-        methods: {
-            logFromChild() {
-                console.log("logged from the component");
-            },
-
-            passEvent() {
-                this.$emit('shoutup');
-            }
-        }
+    const HomePageComponent = {
+        template: "<h2>This is my homepage</h2>"
     }
 
+    const ContactComponent = {
+        template: "<h2>This is the Contact page</h2>"
+    }
+
+    const UsersPageComponent = {
+        template: "<h2>This is the user page</h2>"
+    }
+
+    // routes are the path you're talking through the application
+    // www.google.ca is a route; its the route to the homepage
+    // www.sumpinsumpin/contact is the route to the contact page, ect
+    // each route gets mapped to a component that you define 
+    // and that component gets rendered in the <router-view> element
+
+    const routes = [
+        { path: '/', name: 'home', component: HomePageComponent },
+        { path: '/contact', name: 'contact', component: ContactComponent },
+        { path: '/users', name: 'users', component: UsersPageComponent }
+    ]
+
+    const router = new VueRouter({
+        routes
+    });
+
+    //const UserComponent = {
+    //    props: ['name', 'role'],
+    //
+    //    template: "#userstemplate",
+    //
+    //       created: function() {
+    //          console.log("I'm Aliive");
+    //    },
+//
+    //    methods: {
+    //        logFromChild() {
+    //            console.log("logged from the component");
+    //        },
+//
+    //        passEvent() {
+    //            this.$emit('shoutup');
+    //        }
+    //    }
+   // }
+//
     // Then your vue Instance
 
     const vm = new Vue({
@@ -41,9 +70,13 @@
         },
 
         components: {
-            'activeusers' : UserComponent
-        }
-    })
+            'homepagecomponent' : HomePageComponent,
+            'contactcomponent' : ContactComponent,
+            'userscomponent' : UsersPageComponent
+        },
+
+        router: router
+    });
 
 
 })()
